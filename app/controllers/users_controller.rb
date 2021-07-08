7dd209b14dd7106a3d84e3ee71e207f08510a7fb
Yaro <yshmarov@gmail.com>
2021-07-08 11:37:13 +0000

    basic gem rolify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(*User::ROLES)
+    params.require(:user).permit({role_ids: []})
   end
 end
