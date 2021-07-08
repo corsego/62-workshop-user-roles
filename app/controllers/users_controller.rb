3a628b1d665fae6f1e8146d7825cd45e6199984a
Yaro <yshmarov@gmail.com>
2021-07-08 11:25:52 +0000

    add roles to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.update(user_params)
+      redirect_to users_url, notice: "User was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(*User::ROLES)
+  end
 end
